6b6e116df42e8be736359ca1d3bd0b35b5d0febc
AntonioTagliafierro <anto.tagliafierro@studenti.unina.it>
2024-02-11 00:41:16 +0100

    aggiunti i bottom buttons alla library

--- app/src/main/java/com/project/thelibrarians_lso2324/activities/LibraryActivity.java
+++ app/src/main/java/com/project/thelibrarians_lso2324/activities/LibraryActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.GridLayout;
 import android.widget.GridView;
 import android.widget.SearchView;
 import android.widget.ImageView;
@@ -22,7 +23,7 @@ public class LibraryActivity extends AppCompatActivity {
     private BookAdapter bookAdapter;
     private ArrayList<Book> bookList;
     private GridLayout bookGridLayout;
-    private ArrayList<Book> bookList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
